7f8bac6b4d1506965410eb04e9512ea0e31bb56e
Martin-Alexander <martingianna@gmail.com>
2017-08-05 22:23:22 -0400

    very basic file system for proper loading order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,15 @@
 //= require jquery
 //= require turbolinks
-//= require_tree .
+//= require_tree ./channels
+//= require cable
+//= require_tree ./initialization
+//= require_tree ./game
+
+/*
+
+Order of loading
+
+1 - Globals variables and objects: "./initializatio"
+2 - Everything else: "./game"
+
+*/
