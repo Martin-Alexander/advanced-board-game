9730dd8d4fb5e759a3e9159167c19f1f21ef2a69
Martin-Alexander <martingianna@gmail.com>
2017-08-06 16:15:02 -0400

    a mouse tracking event listener is added in the setup file and updates the hand object whenever the mouse is moved and on click

---
+++ app/assets/javascripts/initialization/event_listeners.js
@@ -0,0 +1,25 @@
+function initializeMouseListener() {
+
+  canvas.addEventListener("mousemove", function(event) {
+
+    var rect = canvas.getBoundingClientRect();
+    hand.mousePosition = {
+      x: event.clientX - rect.left - 1100 / 2,
+      y: event.clientY - rect.top - 50
+    };
+    hand.mouseIsoPosition = {
+      x: (hand.mousePosition.x + 2 * hand.mousePosition.y) / 2,
+      y: (2 * hand.mousePosition.y - hand.mousePosition.x) / 2
+    }
+    hand.hoverTile = {
+      x: Math.floor((hand.mouseIsoPosition.x) / tileHeight),
+      y: Math.floor((hand.mouseIsoPosition.y) / tileHeight)
+    }
+  });
+
+  canvas.addEventListener("mouseup", function() {
+  
+    hand.click();
+
+  });
+}
