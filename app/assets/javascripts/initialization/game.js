b791f906f104e9b235d8ce632f64c455652d265b
Martin-Alexander <martingianna@gmail.com>
2017-08-06 03:06:41 -0400

    primitive new game generation method to 'game'

--- app/assets/javascripts/initialization/game.js
+++ app/assets/javascripts/initialization/game.js
@@ -2,12 +2,25 @@ var game = {
   turnNumber: 0,
   playerOne: undefined,
   playerTwo: undefined,
-  globalBoard: [],
   turnplayer: function() {
     if (this.playerOne.isTurnPlayer) {
       return this.playerOne;
     } else {
       return this.playerTwo;
+    }
+  }
+}
+
+game.generateNewBoard = function() {
+  this.globalBoard.empty();
+  for (var y = 0; y < ySize; y++) {
+    for (var x = 0; x < xSize; x++) {
+      var newSquare = new Square(x, y);
+      newSquare.board = this.globalBoard.data;
+      newSquare.player = null;
+      newSquare.terrain = "grass";
+      newSquare.structure = null;
+      this.globalBoard.data.push(newSquare);
     }
   }
 }
