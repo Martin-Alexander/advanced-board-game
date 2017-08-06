e4bd470ab4246e6f5da33e570f3b1fade3b54d04
Martin-Alexander <martingianna@gmail.com>
2017-08-06 03:07:25 -0400

    primative board render function, improved 'drawFromInput' that uses lookup table

--- app/assets/javascripts/game/render.js
+++ app/assets/javascripts/game/render.js
@@ -1,9 +1,9 @@
-function drawFromSource(x, y) {
+function drawFromSource(input, x, y) {
   canvasContext.save();
   canvasContext.translate((x - y) * (tileWidth / 2 + 1), (x + y) * tileHeight / 2);
   canvasContext.drawImage(
     sourceImage, 
-    1 + (65 * x), 1 + (49 * y), tileWidth, 49,
+    1 + (65 * drawFromSourceLookup[input].x), 1 + (49 * drawFromSourceLookup[input].y), tileWidth, 49,
     -tileWidth / 2, 0, tileWidth, 49
   );
   canvasContext.restore();
@@ -14,4 +14,17 @@ function clearCanvas() {
   canvasContext.setTransform(1, 0, 0, 1, 0, 0);
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   canvasContext.restore();
+}
+
+function renderBoard() {
+  for (var i = 0; i < xSize * ySize; i++) {
+    var square = game.globalBoard.data[i];
+    drawFromSource("water", square.x, square.y);
+  }
+}
+
+var drawFromSourceLookup = {
+  grass: { x: 2, y: 0},
+  water: { x: 3, y: 0},
+  mountain: { x: 4, y: 0}
 }
