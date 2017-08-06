81e806465798ec8966b593d82b0893127dbeb145
Martin-Alexander <martingianna@gmail.com>
2017-08-06 03:06:01 -0400

    simple function to create a new game

--- app/assets/javascripts/game/new_game.js
+++ app/assets/javascripts/game/new_game.js
@@ -1,4 +1,27 @@
 // Takes the 'game' object and initializes as a brand new game state
 function newGame() {
 
+  // Back to turn 1
+  game.turnNumber = 1;
+
+  // Game gets a board
+  game.globalBoard = new Board;
+
+  // Set players
+  game.playerOne = new Player;
+  game.playerOne.id = 1;
+  game.playerOne.isTurnPlayer = true;
+  game.playerOne.gold = 0;
+  game.playerOne.vision = new Board;
+  game.playerOne.blind();
+
+  game.playerTwo = new Player;
+  game.playerTwo.id = 2;
+  game.playerTwo.isTurnPlayer = false;
+  game.playerTwo.gold = 0;
+  game.playerTwo.vision = new Board;
+  game.playerTwo.blind();
+
+  // A randomly generated board
+  game.generateNewBoard();
 }
