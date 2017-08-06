5268a654080882b2bf552b3e6982df52992d196f
Martin-Alexander <martingianna@gmail.com>
2017-08-06 02:35:34 -0400

    added a blind() method to players (main for initialization)

--- app/assets/javascripts/initialization/player.js
+++ app/assets/javascripts/initialization/player.js
@@ -4,3 +4,14 @@ function Player() {
   this.gold;
   this.vision;
 }
+
+Player.prototype.blind = function blindPlayer(player) {
+  for (var y = 0; y < ySize; y++) {
+    for (var x = 0; x < xSize; x++) {
+      var newVisionSquare = new VisionSquare(x, y);
+      newVisionSquare.status = "black";
+      newVisionSquare.board = player.vision;
+      player.vision.data.push << newVisionSquare;
+    }
+  }
+}
