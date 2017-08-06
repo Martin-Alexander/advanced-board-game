9724da86345f310c1a9bf159f7bc47ac2606f947
Martin-Alexander <martingianna@gmail.com>
2017-08-05 22:33:33 -0400

    some global variables

---
+++ app/assets/javascripts/initialization/globals.js
@@ -0,0 +1,15 @@
+// Global variables
+
+const xSize = 18;
+const ySize = 18;
+const tildWidth = 0;
+const tildWidth = 0;
+
+// Images Sources
+
+// TODO
+
+// const terrainImage = ;
+// const unitImage = ;
+// const structureImage = ;
+// const elementImage = ;
