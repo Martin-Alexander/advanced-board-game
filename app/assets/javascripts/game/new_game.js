401322f1de73d3d5e40f845fab70f27d3a823009
Martin-Alexander <martingianna@gmail.com>
2017-08-06 04:44:00 -0400

    bug fix

--- app/assets/javascripts/game/new_game.js
+++ app/assets/javascripts/game/new_game.js
@@ -9,14 +9,14 @@ function newGame() {
 
   // Set players
   game.playerOne = new Player;
-  game.playerOne.id = 1;
+  game.playerOne.number = 1;
   game.playerOne.isTurnPlayer = true;
   game.playerOne.gold = 0;
   game.playerOne.vision = new Board;
   game.playerOne.blind();
 
   game.playerTwo = new Player;
-  game.playerTwo.id = 2;
+  game.playerTwo.number = 2;
   game.playerTwo.isTurnPlayer = false;
   game.playerTwo.gold = 0;
   game.playerTwo.vision = new Board;
