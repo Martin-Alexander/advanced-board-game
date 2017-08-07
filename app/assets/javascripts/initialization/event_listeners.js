46478e9a856cafab083af97ea31bf611cda45b48
Martin-Alexander <martingianna@gmail.com>
2017-08-07 19:00:37 -0400

    added double click debugging tool

--- app/assets/javascripts/initialization/event_listeners.js
+++ app/assets/javascripts/initialization/event_listeners.js
@@ -1,7 +1,44 @@
 function initializeMouseListener() {
 
-  canvas.addEventListener("mousemove", function(event) {
+  canvas.addEventListener("dblclick", function(event) {
     
+    var rect = canvas.getBoundingClientRect();
+    var trueMousePosition = {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+    }
+
+    var mousePosition = {
+      x: ((event.clientX - rect.left - canvasWidth / 2)) - hand.offset.x,
+      y: (event.clientY - rect.top + tileHeight / 2 - 34) - hand.offset.y
+    };
+
+    var mouseIsoPosition = {
+      x: (mousePosition.x + 2 * mousePosition.y) / 2,
+      y: (2 * mousePosition.y - mousePosition.x) / 2
+    }
+
+    if (
+      Math.floor((mouseIsoPosition.x) / tileHeight) >= 0 &&
+      Math.floor((mouseIsoPosition.x) / tileHeight) < xSize &&
+      Math.floor((mouseIsoPosition.y) / tileHeight) >= 0 &&
+      Math.floor((mouseIsoPosition.y) / tileHeight) < ySize
+    ) {
+      var hoverTile = {
+        x: Math.floor((mouseIsoPosition.x) / tileHeight),
+        y: Math.floor((mouseIsoPosition.y) / tileHeight)
+      }
+    } else {
+      var hoverTile = undefined;
+    }
+
+    if (hoverTile) {
+      console.log(game.globalBoard.square(hoverTile.x, hoverTile.y));
+    }
+  });
+  
+  canvas.addEventListener("mousemove", function(event) {
+
     setMouseInput();
   });
 
@@ -10,6 +47,7 @@ function initializeMouseListener() {
     hand.click();
     setMouseInput();
   });
+
 }
 
 function setMouseInput() {
