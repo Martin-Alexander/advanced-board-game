36e72b699a78e481aca9df66b86a8920d2986dee
Martin-Alexander <martingianna@gmail.com>
2017-08-06 22:11:13 -0400

    removed all margins and padding. canvas sits snuggly against the boarders of the browser

--- app/assets/javascripts/initialization/globals.js
+++ app/assets/javascripts/initialization/globals.js
@@ -5,5 +5,5 @@ const ySize = 40;
 const tileWidth = 64;
 const tileHeight = 32;
 
-const canvasWidth = window.innerWidth - 20;
-const canvasHeight = window.innerHeight - 20;
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight;
