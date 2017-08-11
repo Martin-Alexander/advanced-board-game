d703784780f5336a3de8cae3da703a33ef5d20c0
Martin-Alexander <martingianna@gmail.com>
2017-08-11 18:44:50 -0400

    basic routing for database interaction

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,7 @@ class MainController < ApplicationController
   end
 
   def input
+    Game.first.data = params[:game]
     ActionCable.server.broadcast "game_channel", {
       playerNumber: params[:playerNumber],
       game: params[:game],
@@ -19,5 +20,32 @@ class MainController < ApplicationController
       fromDamage: params[:fromDamage],
       toDamage: params[:toDamage]
     }
-  end    
+  end
+
+  def join_game
+    role = params[:role]
+    player_one = Game.first.player_one
+    player_two = Game.first.player_two
+
+    if role == "observer"
+      join_as("observer")
+    elsif role == "playerOne"
+      player_one ? join_as("playerOne", player_one) : join_as("observer", player_one)
+    elsif role == "playerTwo"
+      player_two ? join_as("playerTwo", player_two) : join_as("observer", player_two)
+    end
+  end
+
+  def game_over
+    Game.first.player_one = false
+    Game.first.player_two = false
+    Game.first.data = ""
+  end
+
+  def join_as(status, other_player_status) 
+    ActionCable.server.broadcast "game_channel", {
+      joinAs: status,
+      otherPlayer: other_player_status
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'main#home'
   post '/input', to: 'main#input'
   post '/damage', to: 'main#damage'
+  post '/join_game', to: 'main#join_game'
+  post '/game_over', to: 'main#game_over'
 end
